77be64d598c3e699dfebc454c8515c98985ac383
91818 <91818@LAPTOP-OMCDS4TQ>
2022-02-03 22:00:40 +0530

    com.javabasics package added to project name

---
+++ src/com/javabasics/Example1.java
@@ -0,0 +1,34 @@
+package com.javabasics;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int LearnerId = 449 ;
+	      String  LearnerName ="TEJU";
+	      int   LearnerAge =23;
+	      String  LearnerAddress = "Hyderabad";
+	      float  LearnerMarks =70.50f;
+	      boolean  LearnerResult = true;
+	      
+	      
+	      System.out.print("Learner Id");
+	      System.out.println(LearnerId);
+	      
+	      System.out.print("Learner Name");
+	      System.out.println(LearnerName);
+	      
+	      System.out.print("Learner Age");
+	      System.out.println(LearnerAge);
+	      
+	      System.out.print("Learner Address");
+	      System.out.println(LearnerAddress);
+	      
+	      System.out.print("Learner Marks");
+	      System.out.println(LearnerMarks);
+	      
+	      System.out.print("Learner Result(true means pass false means fail):");
+	      System.out.println(LearnerResult);
+	}
+
+}
